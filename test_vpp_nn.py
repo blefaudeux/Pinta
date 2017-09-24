76c14e7f436c8b7c76e89a1c94fd27867fa9809d
Benjamin Lefaudeux <benjamin.lefaudeux@oculus.com>
2017-09-23 21:50:36 -0700

    keras v2 changes + conv network

--- test_vpp_nn.py
+++ test_vpp_nn.py
@@ -3,7 +3,7 @@ import numpy as np
 from data_processing import plot as plt
 from data_processing.split import split
 from data_processing.load import load
-from train.behaviour import SimpleNN, MemoryNN
+from train.behaviour import ConvNN, MemoryNN
 
 # Load the dataset
 datafile = 'data/31_08_2016.json'
@@ -17,15 +17,15 @@ plt.parrallel_plot([df[i] for i in data_plot], data_plot, "Dataset plot")
 # Split in between training and test
 inputs = ['wind_speed', 'wind_angle', 'rudder_angle']
 training_ratio = 0.67
-train_in, train_out, test_in, test_out = split(df, inputs,
-                                               ['boat_speed'],
-                                               training_ratio)
+train_in, train_out, test_in, test_out = split(
+    df, inputs, ['boat_speed'], training_ratio
+)
 
 # Super basic NN, FW stateless NN
 name_simple = "trained/simple_nn.hf5"
-snn = SimpleNN(name_simple)
+snn = ConvNN(name_simple)
 if not snn.valid:
-    snn.fit(train_in, train_out, nb_epoch=50, batch_size=1, verbose=2)
+    snn.fit(train_in, train_out, epoch=50, batch_size=16, verbose=2)
     snn.save(name_simple)
 
 trainScore = snn.evaluate(train_in, train_out, verbose=0)
@@ -36,14 +36,14 @@ print('Test Score: %.2f RMSE' % np.sqrt(testScore))
 
 # Test a more complex NN, LSTM
 train_inputs_ltsm = np.reshape(
-    train_in, (train_in.shape[0], 1, train_in.shape[1]))
+    train_in, (train_in.shape[0], 1, train_in.shape[1])
+)
 test_inputs_ltsm = np.reshape(test_in, (test_in.shape[0], 1, test_in.shape[1]))
 
 name_lstm = "trained/lstm_nn.hf5"
 mnn = MemoryNN(name_lstm)
 if not mnn.valid:
-    mnn.fit(train_inputs_ltsm, train_out,
-            batch_size=100, nb_epoch=200, verbose=2)
+    mnn.fit(train_inputs_ltsm, train_out, batch_size=100, epoch=200, verbose=2)
     mnn.save(name_lstm)
 
 trainScore = mnn.evaluate(train_inputs_ltsm, train_out, verbose=0)
@@ -57,8 +57,10 @@ print('---\nQuality evaluation:')
 pred_simple = snn.predict(test_in).flatten()
 pred_ltsm = mnn.predict(test_inputs_ltsm).flatten()
 
-plt.parrallel_plot([test_out.flatten(), pred_ltsm, pred_simple],
-                   ["Ground truth", "LTSM", "Simple NN"],
-                   "Neural network predictions vs ground truth")
+plt.parrallel_plot(
+    [test_out.flatten(), pred_ltsm,
+     pred_simple], ["Ground truth", "LTSM", "Simple NN"],
+    "Neural network predictions vs ground truth"
+)
 
 print('--Done')
--- train/behaviour.py
+++ train/behaviour.py
@@ -1,13 +1,11 @@
 #!/usr/local/bin/python3
-import tensorflow as tf   # Bugfix in between Keras and TensorFlow
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.models import Sequential, load_model
-# tf.python.control_flow_ops = tf
+from keras.layers.convolutional import Conv1D, Conv3D
 
 
 class NN():
-
     def __init__(self):
         self._model = None  # We keep the actual NN private, safer
         self.valid = False
@@ -26,12 +24,16 @@ class NN():
     def save(self, name):
         self._model.save(name)
 
-    def fit(self, train_in, train_out,
-            nb_epoch=50, batch_size=1, verbose=2):
+    def fit(self, train_in, train_out, epoch=50, batch_size=1, verbose=2):
 
         print("Training the network...")
-        self._model.fit(train_in, train_out, nb_epoch=nb_epoch,
-                        batch_size=batch_size, verbose=verbose)
+        self._model.fit(
+            train_in,
+            train_out,
+            epoch=epoch,
+            batch_size=batch_size,
+            verbose=verbose
+        )
         self.valid = True
         print("... Done")
 
@@ -43,32 +45,55 @@ class NN():
         return self._model.evaluate(inputs, outputs, verbose=verbose)
 
 
-class SimpleNN(NN):
-
+class ConvNN(NN):
     def __init__(self, filename=None):
-        super(SimpleNN, self).__init__()
+        super(ConvNN, self).__init__()
 
         # Load from trained NN if required
         if filename is not None:
-            # TODO: Catch load failure ?
             self.valid = self.load(filename)
             if self.valid:
                 return
+            else:
+                print(
+                    "Could not load the specified net, computing it from scratch"
+                )
 
         # Else define the _model
         hidden_neurons = 16
+        nb_filters = 16
+        filter_length = 10
         self._model = Sequential()
-        self._model.add(Dense(hidden_neurons, input_dim=3))
-        self._model.add(Dense(hidden_neurons, activation='relu'))
-        self._model.add(Dense(hidden_neurons, activation='relu'))
+        self._model.add(
+            Conv1D(
+                filters=nb_filters,
+                kernel_size=filter_length,
+                activation='relu',
+                input_shape=(None, 3)
+            )
+        )
+        self._model.add(Activation('sigmoid'), input_dim=nb_filters)
+        self._model.add(MaxPooling1D(pool_length=4))
+        self._model.add(
+            Convolution1D(nb_filter=32, filter_length=8, border_mode='valid')
+        )
+        self._model.add(Activation('sigmoid'))
+        self._model.add(MaxPooling1D(pool_length=4))
+        self._model.add(Flatten())
+        self._model.add(Dense(hidden_neurons))
         self._model.add(Dense(1, input_dim=hidden_neurons, activation='linear'))
-        self._model.compile(loss='mean_squared_error', optimizer='adam')
-        print("Simple NN defined as follows\n \
-          {}".format(str(self._model.summary())))
+        self._model.compile(
+            loss='mean_squared_error',
+            optimizer='adam',
+            sample_weight_mode='temporal'
+        )
+        print(
+            "Conv NN defined as follows\n \
+          {}".format(str(self._model.summary()))
+        )
 
 
 class MemoryNN(NN):
-
     def __init__(self, filename=None):
         super(MemoryNN, self).__init__()
 
@@ -87,7 +112,6 @@ class MemoryNN(NN):
         self._model.add(Dense(hidden_neurons, activation='relu'))
         self._model.add(Dense(hidden_neurons, activation='relu'))
         self._model.add(Dense(hidden_neurons, activation='relu'))
-        self._model.add(
-            Dense(1, input_dim=hidden_neurons, activation='linear'))
+        self._model.add(Dense(1, input_dim=hidden_neurons, activation='linear'))
 
         self._model.compile(loss="mean_squared_error", optimizer="rmsprop")
