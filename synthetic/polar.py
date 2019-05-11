partially fixed the save and load, but data specific metrics -like normalization- are not available when loading. Would need to be part of a settings file for instance

synthetic/polar.py
@@ -5,19 +5,16 @@ from collections import namedtuple
 SpeedPolarPoint = namedtuple(
     "SpeedPolarPoint", ["wind_angle",  "wind_speed", "rudder_angle", "boat_speed"])
 
-# Generate data points across the whole range
 
-
-def generate_data(engine, wind_range, wind_step, angular_step):
+def generate(engine, wind_range, wind_step, angular_step):
     speed = []
 
     for w in range(wind_range[0], wind_range[1], wind_step):
-        for a in range(0, np.pi, angular_step):
-            pt = SpeedPolarPoint()
-            pt.wind_angle = a
-            pt.wind_speed = w
-            pt.rudder_angle = 0.
-            pt.boat_speed = engine.predict(w, a, 0.)
+        for a in np.arange(0., np.pi, angular_step):
+            pt = SpeedPolarPoint(wind_angle=a,
+                                 wind_speed=w,
+                                 rudder_angle=0.,
+                                 boat_speed=engine.predict(w, a, 0.))
             speed.append(pt)
 
     return speed

train/engine.py
@@ -32,13 +32,16 @@ class NN(nn.Module):
     def __init__(self, logdir):
         super(NN, self).__init__()
         self.model = None
-        self.valid = False
+        self._valid = False
         self.mean = None
         self.std = None
 
         # Set up TensorBoard
         self.summary_writer = SummaryWriter(logdir)
         self.summary_writer.add_graph(self.model)
+
+    def valid(self):
+        return self._valid
 
     def save(self, name):
         with open(name, "wb") as f:
@@ -132,7 +135,8 @@ class NN(nn.Module):
                 def closure():
                     optimizer.zero_grad()
                     out, _ = self(batch_data.input)
-                    loss = criterion(out, batch_data.output.view(out.size()[0], -1))
+                    loss = criterion(
+                        out, batch_data.output.view(out.size()[0], -1))
                     print('Train loss: {:.4f}'.format(loss.item()))
                     self.summary_writer.add_scalar('train', loss.item(), i)
                     loss.backward()
@@ -142,7 +146,8 @@ class NN(nn.Module):
 
                 # Loss on the test data
                 pred, _ = self(test_seq.input)
-                loss = criterion(pred, test_seq.output.view(pred.size()[0], -1))
+                loss = criterion(
+                    pred, test_seq.output.view(pred.size()[0], -1))
                 self.summary_writer.add_scalar('test', loss.item(), i)
                 print("Test loss: {:.4f}\n".format(loss.item()))
 

train/engine_cnn.py
@@ -6,7 +6,7 @@ import numpy as np
 import logging
 from tensorboardX import SummaryWriter
 
-from train.behaviour import NN
+from train.engine import NN
 
 dtype = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
 
@@ -18,16 +18,6 @@ class Conv(NN):
 
     def __init__(self, logdir, input_size, hidden_size, filename=None):
         super(Conv, self).__init__(logdir)
-
-        # Load from trained NN if required
-        if filename is not None:
-            self.valid = self.load(filename)
-            if self.valid:
-                return
-
-            print(
-                "Could not load the specified net, computing it from scratch"
-            )
 
         # ----
         # Define the model
@@ -57,12 +47,20 @@ class Conv(NN):
             print("Using Pytorch CUDA backend")
             self.cuda()
 
+        # Load from trained NN if required
+        if filename is not None and self.load(filename):
+            self._valid = True
+            return
+
+        print("Could not load the specified net, \
+                needs to be computing from scratch")
+
     def load(self, filename):
         try:
             with open(filename, "rb") as f:
                 self.load_state_dict(torch.load(f))
                 print("---\nNetwork {} loaded".format(filename))
-                print(self.model.summary())
+                print(self)
                 return True
 
         except (ValueError, OSError, IOError, TypeError) as e: