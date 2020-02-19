1711da4c35ad5d63fdcc503ad48cb6959c617252
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2020-02-18 21:40:50 -0800

    minor, more modern print

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 from functools import wraps
 from time import time
 
+
 def timing(f):
     @wraps(f)
     def wrap(*args, **kw):
         ts = time()
         result = f(*args, **kw)
         te = time()
-        print('func:%r args:[%r, %r] took: %2.4f sec' % \
-          (f.__name__, args, kw, te-ts))
+        print('{} took: {:2.4f} sec'.format(f.__name__, te-ts))
         return result
     return wrap
