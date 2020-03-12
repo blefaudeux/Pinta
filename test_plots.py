31d442dcd0d5e0615314d277947bdab51ac12981
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2020-03-11 17:23:01 -0700

    fixing the test plots

--- test_plots.py
+++ test_plots.py
@@ -2,9 +2,10 @@
 
 from data_processing.plot import multi_plot, speed_plot
 from data_processing.load import load
+from pathlib import Path
 
 # Load the full json and do some analysis
-df = load('data/03_09_2016.json', clean_data=True, whiten_data=True)[0]
+df = load(Path('data/03_09_2016.json'), clean_data=True)
 
 # Purely sequential plot
 multi_plot(df, ['rudder_angle', 'wind_speed', 'wind_angle', 'boat_speed'],
@@ -12,8 +13,3 @@ multi_plot(df, ['rudder_angle', 'wind_speed', 'wind_angle', 'boat_speed'],
 
 # Polar plot
 speed_plot(df, decimation=2, filename="speed_polar")
-
-# Test an effect of the rudder angle on speed
-# TODO: Create a plot wrapper for this, correlation style
-# df.plot(x=df['boat_speed'][:-1]-df['boat_speed'][1:],
-#         y=df['rudder_angle'].iloc[:-1], style='*')
