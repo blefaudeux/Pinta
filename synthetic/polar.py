0dd0196ff61caf194128e0abbd03e1bca7970b85
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2020-04-18 22:02:46 -0700

    synthetic polar needs fixing

--- synthetic/polar.py
+++ synthetic/polar.py
@@ -12,20 +12,22 @@ SpeedPolarPoint = namedtuple(
 def generate(engine, wind_range, wind_step, angular_step, seq_len):
     speed = []
 
+    # Build an artificial dataloader
+    # FIXME: Ben
     for w in range(wind_range[0], wind_range[1], wind_step):
         for a in np.arange(0.0, np.pi, angular_step):
-            sample_input = TrainingSample(
-                np.array([w, np.cos(a), np.sin(a), 0.0]), np.array([0.0])
+            _ = TrainingSample(
+                inputs=np.array([w, np.cos(a), np.sin(a), 0.0]), outputs=np.array([0.0])
             )
 
-            # FW pass in the DNN and save
-            pred = engine.predict(sample_input, seq_len)
-            pred = pred.detach().cpu().numpy()
+    # FW pass in the DNN and save
+    # FIXME: Ben. Broken interface. Would need unit testing
 
-            speed.append(
-                SpeedPolarPoint(
-                    wind_angle=a, wind_speed=w, rudder_angle=0.0, boat_speed=pred
-                )
-            )
+    # pred = engine.predict(sample_input, seq_len)
+    # pred = pred.detach().cpu().numpy()
+
+    # speed.append(
+    #     SpeedPolarPoint(wind_angle=a, wind_speed=w, rudder_angle=0.0, boat_speed=pred)
+    # )
 
     return speed
