Fix transform and scheduler, average validations

data_processing/transforms.py
@@ -66,7 +66,7 @@ class Normalize:
 
     def __call__(self, sample: TrainingSample):
         # Non batched data
-        if len(sample.inputs.shape) == 2:
+        if sample.inputs.shape[0] > sample.inputs.shape[1]:
             return TrainingSample(
                 inputs=torch.div(
                     torch.add(sample.inputs, -self.mean.inputs.reshape(1, -1)),

train/engine.py
@@ -87,6 +87,8 @@ class NN(nn.Module):
 
             self.log.info("***** Epoch %d", i_epoch)
 
+            validation_loss = torch.zeros(1)
+
             for i_batch, (train_batch, test_batch) in enumerate(zip(trainer, tester)):
                 # Eval computation on the training data
                 def closure_train(data=train_batch):
@@ -117,7 +119,7 @@ class NN(nn.Module):
                     return loss
 
                 optimizer.step(closure_train)
-                validation_loss = closure_validation()
+                validation_loss += closure_validation()
 
                 i_log += 1
 