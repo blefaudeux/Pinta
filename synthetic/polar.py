880a544655f9dbbc405a5645bf5733e530dc6aa8
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2020-04-23 21:34:34 -0700

    Fixing the polar plots, but now the result looks a bit funky, could be a faulty model

--- synthetic/polar.py
+++ synthetic/polar.py
@@ -1,47 +1,79 @@
 from collections import namedtuple
+from typing import List
 
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
-from data_processing.training_set import TrainingSet
+from data_processing.training_set import TrainingSample, TrainingSet, TrainingSetBundle
+from data_processing.transforms import Normalize
 
 SpeedPolarPoint = namedtuple(
     "SpeedPolarPoint", ["wind_angle", "wind_speed", "rudder_angle", "boat_speed"]
 )
 
 
-def generate(engine, wind_range, wind_step, angular_step, seq_len):
+def generate(
+    engine,
+    wind_range: List[int],
+    wind_step: int,
+    angular_step: float,
+    seq_len: int,
+    mean: TrainingSample,
+    std: TrainingSample,
+):
     # Build an artificial TrainingSet
     # - one single measurement point per polar coord
     # (repeat the same inputs for the whole sequence)
-    inputs = []
-    outputs = []
+    datasets = []
+
+    normalizer = Normalize(
+        mean.to(torch.device("cpu"), torch.float),
+        std.to(torch.device("cpu"), torch.float),
+    )
 
     for w in range(wind_range[0], wind_range[1], wind_step):
         for a in np.arange(0.0, np.pi, angular_step):
-            inputs.append(
-                torch.tensor([w, np.cos(a), np.sin(a), 0.0]).repeat(seq_len, 1)
+            # Generate the sample we want for this point in the polar plot
+            # Expected size for transform is [Time x Channels],
+            # so we unsqueeze front
+            sample = TrainingSample(
+                inputs=torch.tensor([w, np.cos(a), np.sin(a), 0.0]).unsqueeze(0),
+                outputs=torch.zeros(1, 1),
             )
-            outputs.append(torch.zeros(1, seq_len))
+
+            # Normalize to get to the range the model has learnt
+            datasets.append(
+                TrainingSet.from_training_sample(normalizer(sample), seq_len)
+            )
 
     # - build a collection of unrelated points from this,
     # a TrainingSetBUndle
-    dataset = TrainingSet(torch.stack(inputs).permute(0, 2, 1), torch.stack(outputs))
+    dataset_bundle = TrainingSetBundle(datasets)
 
     # - get a dataloader from the ad-hoc dataset
-    dataloader = DataLoader(dataset, batch_size=100, shuffle=False)
+    dataloader = DataLoader(
+        dataset_bundle.get_training_set(seq_len)[0], batch_size=100, shuffle=False
+    )
 
-    # FW pass in the DNN and save
-    pred = engine.predict(dataloader, seq_len)
+    # FW pass in the DNN.
+    # Passing in the mean and std allows for de-normalization on the way out
+    pred = engine.predict(dataloader, mean.outputs, std.outputs)
     pred = pred.detach().cpu().numpy()
 
-    # TODO:
-    # - unpack the results so that they fit the expected format for plotting
-    # - handle de-normalization
+    # Unpack, generate the appropriate list of points for plotting
+    i_pred = 0
+    speeds = []
+    for w in range(wind_range[0], wind_range[1], wind_step):
+        for a in np.arange(0.0, np.pi, angular_step):
+            speeds.append(
+                SpeedPolarPoint(
+                    wind_angle=a,
+                    wind_speed=w,
+                    rudder_angle=0.0,
+                    boat_speed=pred[i_pred],
+                )
+            )
+            i_pred += 1
 
-    # speed.append(
-    #     SpeedPolarPoint(wind_angle=a, wind_speed=w, rudder_angle=0.0, boat_speed=pred)
-    # )
-
-    # return speed
+    return speeds
