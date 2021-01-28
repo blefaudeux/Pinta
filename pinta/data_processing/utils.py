38d2462d5b5ca89ba0436e619c276826cf2dd6af
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-01-27 21:13:03 -0800

    adding all the settings, switch json load to rapidjson

--- pinta/data_processing/utils.py
+++ pinta/data_processing/utils.py
@@ -1,4 +1,4 @@
-import json
+import rapidjson
 from pathlib import Path
 
 import pandas as pd
@@ -9,7 +9,7 @@ def save_json(dataframe: pd.DataFrame, filepath: Path):
         filepath = Path(filepath)
 
     with filepath.open("w") as file_out:
-        json.dump(dataframe.to_json(), file_out)
+        rapidjson.dump(dataframe.to_json(), file_out)
 
 
 def load_json(filepath: Path, skip_zeros=True) -> pd.DataFrame:
@@ -17,7 +17,7 @@ def load_json(filepath: Path, skip_zeros=True) -> pd.DataFrame:
         filepath = Path(filepath)
 
     with filepath.open("r") as file_in:
-        data = json.load(file_in)
+        data = rapidjson.load(file_in)
 
     dataframe = pd.read_json(data)
     return dataframe if not skip_zeros else dataframe[dataframe.sog > 0].dropna()
