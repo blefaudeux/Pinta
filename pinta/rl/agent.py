01be6f155395ecda3e5563d1d45ec5c876634865
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2021-04-21 21:25:27 -0700

    starting the work with rl agents and envs

---
+++ pinta/rl/agent.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+class Net(nn.Module):
+    def __init__(self, obs_size: int, hidden_size: int, n_actions: int):
+        super(Net, self).__init__()
+        self.net = nn.Sequential(
+            nn.Linear(obs_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, n_actions)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def get_agent(obs_size: int, hidden_size: int, n_actions: int):
+    return Net(obs_size, hidden_size, n_actions)
